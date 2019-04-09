0d2e87f0bad237ba9da03632641b7d83cc1e3a63
Tomas Haas <xhaast00@stud.fit.vutbr.cz>
2019-04-09 19:36:53 +0200

    WEBAPI: first try

---
+++ currency_converter.py
@@ -0,0 +1,15 @@
+print("Ar.")
+
+TESTOBJECT = {
+    "input": {
+        "amount": 0.9,
+        "currency": "CNY"
+    },
+    "output": {
+        "AUD": 0.20,
+        "CZK": "moc",
+    }
+}
+
+def test():
+    return TESTOBJECT
